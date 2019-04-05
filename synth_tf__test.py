b50f35dcee00cad91a3dac51b54a223c4b566ff6
bvm84 <vadim-boss@mail.ru>
2019-04-05 18:57:24 +0300

    Need to wint ndf_test normalztion.

--- synth_tf__test.py
+++ synth_tf__test.py
@@ -5,12 +5,9 @@ import numpy as np
 from pandas import DataFrame, Series
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from keras.layers import Dense, Input, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.layers import Dense, Input
 from keras.models import Model
 from keras.callbacks import Callback
-from keras.preprocessing.image import array_to_img, img_to_array, load_img
-from keras import backend as K
 from arff2pandas import a2p
 
 
@@ -34,36 +31,32 @@ class TerminateOnBaseline(Callback):
 
 class CreateDf():
     def __init__(self):
-        self.df = DataFrame(dtype=object)
+        self.df = None
 
     def create_df_from_folders(self, folders, anotations):
         if len(anotations) == len(folders):
-            last_index_name = ''
-            for anot in anotations:
-                last_index_name = last_index_name + str(anot) + ','
-            last_index_name = 'label@{{{0}}}'.format(last_index_name[:-1])
-            print(last_index_name)
             i = 0
+            arrays_list = []
+            anotation_list = []
             for folder in folders:
-                j = 0
                 for item in glob.iglob(str(folder) + '/*.npy'):
-                    array_data = np.load(item)
-                    indexes = ['f' + str(x) + '@NUMERIC' for x in range(len(array_data))]
-                    # print(indexes)
-                    ser = Series(data=array_data, index=indexes)
-                    ser = ser.append(Series(data=anotations[i], index=[last_index_name]))
-                    # print(ser)
-                    self.df = self.df.append(ser, ignore_index=True)
-                    j += 1
-                    if j >= 100:
-                        break
+                    arrays_list.append(np.load(item))
+                    anotation_list.append(anotations[i])
                 i += 1
-            # print(indexes.append(last_index_name))
-            self.df = self.df.reindex(labels=indexes.append(last_index_name), axis='columns')
-            self.df = self.df.astype({last_index_name: 'str'})
-            print(type(self.df.loc[199][-1]))
-            print(self.df)
-            print('Df creation done')
+            columns_names = ['f' + str(x) + '@NUMERIC' for x in range(len(arrays_list[-1]))]
+            self.df = DataFrame(data=arrays_list, columns=columns_names)
+            last_column_name = ''
+            for anot in anotations:
+                last_column_name = last_column_name + str(anot) + ','
+            last_column_name = 'label@{{{0}}}'.format(last_column_name[:-1])
+            # print(last_column_name)
+            self.df.insert(loc=len(self.df.columns), column=last_column_name, value=anotation_list)
+            # print(self.df)
+            # self.df = self.df.reindex(labels=indexes.append(last_index_name), axis='columns')
+            # self.df = self.df.astype({last_index_name: 'str'})
+            # print(type(self.df.loc[199][-1]))
+            # print(self.df)
+            # print('Df creation done')
         else:
             print('Anotations list len should be the same as folders list len')
 
@@ -83,150 +76,127 @@ class CreateDf():
 
 
 class SynthANN():
-    def __init__(self, square_db_folder, triangle_db_folder):
-        series_list = []
-        anotation_list = []
-        for item in glob.iglob(str(square_db_folder) + '/*.npy'):
-            series_list.append(np.load(item))
-            anotation_list.append(0)
-        for item in glob.iglob(str(triangle_db_folder) + '/*.npy'):
-            series_list.append(np.load(item))
-            anotation_list.append(1)
-        self.df = DataFrame({'x': series_list, 'y': anotation_list})
+    def __init__(self, df):
+        self.df = df
         self.train_df = None
         self.test_df = None
-        del series_list, anotation_list
         self.ndf_train_x = None
         self.ndf_train_y = None
-        self.sdf_train_x = None
-        self.sdf_train_y = None
         self.ndf_test_x = None
         self.ndf_test_y = None
+        self.sdf_train_x = None
+        self.sdf_train_y = None
         self.sdf_test_x = None
         self.sdf_test_y = None
-        self.xscale_object = preprocessing.MinMaxScaler(feature_range=(0, 1), copy=True)
-        self.xscale_object = preprocessing.StandardScaler(copy=True, with_mean=True, with_std=True)
+        self.xnscale_object = preprocessing.MinMaxScaler(feature_range=(0, 1), copy=True)
+        self.ynscale_object = preprocessing.MinMaxScaler(feature_range=(0, 1), copy=True)
+        self.xsscale_object = preprocessing.StandardScaler(copy=True, with_mean=True, with_std=True)
 
-    def split_data(self, size, shuffle_bool):
+    def truncate_data(self, rows_to_store):
+        indexes_to_drop = [x for x in range(rows_to_store, len(self.df) - rows_to_store)]
+        self.df.drop(labels=indexes_to_drop, axis='index', inplace=True)
+        # print(self.df)
+
+    def split_data(self, size=0.2, shuffle_bool=False):
         self.train_df, self.test_df = train_test_split(self.df, test_size=size, shuffle=shuffle_bool)
 
     def normalize_df(self):
-        self.ndf_train_x = self.train_df.copy(deep=True)
+        self.ndf_train_x = self.train_df.copy(deep=True).drop(self.train_df.columns[-1], axis='columns')
         self.ndf_train_x[self.ndf_train_x.columns] = (
-            self.xscale_object.fit_transform(self.ndf_train_x[self.ndf_train_x.columns])
+            self.xnscale_object.fit_transform(self.ndf_train_x[self.ndf_train_x.columns])
         )
-        self.test_df_xs = self.test_df.copy(deep=True)
-        self.test_df_xs[self.test_df_xs.columns] = (
-            self.xscale_object.transform(self.test_df_xs[self.test_df_xs.columns])
+        self.ndf_test_x = self.test_df.copy(deep=True).drop(self.test_df.columns[-1], axis='columns')
+        self.ndf_test_x[self.ndf_test_x.columns] = (
+            self.xnscale_object.transform(self.ndf_test_x[self.ndf_test_x.columns])
         )
-        print('Data normalized')
+        arr = self.train_df.copy(deep=True).iloc[:, -1].values.reshape(-1, 1)[:, 0]
+        print(arr)
+        narr = self.ynscale_object.fit_transform(arr)
+        self.ndf_train_y = Series(narr)
+        print(self.ndf_train_y)
+        '''
+        self.ndf_train_y = Series(self.ynscale_object.fit_transform(
+            self.train_df.copy(deep=True).iloc[:, -1].values.reshape(-1, 1)))
+        #print(len(self.ndf_train_y))
+        self.ndf_test_y = self.ynscale_object.transform(
+            self.test_df.copy(deep=True).iloc[:, -1].values.reshape(-1, 1))
+        '''
+    @staticmethod
+    def train_model(train_x, train_y, epochs_to_train):
+        overfitCallback = TerminateOnBaseline(monitor='acc', baseline=1)
+        train_samle_length = len(train_x[0])
+        model_input = Input(shape=(train_samle_length,))
+        model_dense_1 = Dense(96, activation='relu')(model_input)
+        # model_dropout_1 = Dropout(rate=0.5)(model_dense_1)
+        model_dense_2 = Dense(48, activation='relu')(model_dense_1)
+        predict_out = Dense((1), activation='hard_sigmoid')(model_dense_2)
+        model = Model(inputs=model_input, outputs=predict_out)
+        model.compile(optimizer='rmsprop',
+                      loss='binary_crossentropy',
+                      metrics=['accuracy'])
+        history = model.fit(train_x, train_y, epochs=epochs_to_train, callbacks=[overfitCallback])
+        return history, model
 
-    def train_model(train_df, epochs_to_train):
-        # overfitCallback = EarlyStopping(monitor='acc', min_delta=0.00001, mode='min', baseline=0.99, patience=3)
-        overfitCallback = TerminateOnBaseline(monitor='acc', baseline=0.99)
-        train_samle_length = len(train_df.iloc[0][0])
-        # print(train_df.iloc[0][0])
-        model_fft_input = Input(shape=(train_samle_length,))
-        model_fft_dense_1 = Dense(72, activation='relu')(model_fft_input)
-        model_fft_dense_2 = Dense(36, activation='relu')(model_fft_dense_1)
-        predict_out = Dense(1, activation='sigmoid')(model_fft_dense_2)
-        model_fft = Model(inputs=model_fft_input, outputs=predict_out)
-        model_fft.compile(optimizer='rmsprop',
-                          loss='binary_crossentropy',
-                          metrics=['accuracy'])
-        history = model_fft.fit(np.asarray(train_df['x'].tolist()), np.asarray(train_df['y'].tolist()), epochs=epochs_to_train, callbacks=[overfitCallback])
-        # print(history.history)
-        return history, model_fft
+    @staticmethod
+    def predict_model(model, array):
+        predicted = model.predict(np.expand_dims(array, axis=0))
+        return np.round(np.squeeze(predicted))
 
+    def get_model(self):
+        x = self.ndf_train_x.values.astype(dtype='float64')
+        y = self.ndf_train_y.values.astype(dtype='float64')
+        # print(len(x))
+        # print(len(y))
+        _, model = self.train_model(x, y, 10)
+        return model
 
-    def train_1model(x_train, y_train, epochs):
-        # FFT conv layers
-        train_samle_length = len(x_train[0])
-        print(train_samle_length)
-        print(x_train[0].shape)
-        if K.image_data_format() == 'channels_first':
-            input_shape = (3, 393, 393)
-        else:
-            input_shape = (393, 393, 3)
+    def test_model(self, model):
+        # print(normalized_test_df)
+        test_y = self.ndf_test_x.values
+        test_x = self.ndf_test_y.values
+        scores = model.evaluate(x=test_x, y=test_y, verbose=1)
+        return scores
 
-        model_input = Input(shape=input_shape)
-        # model_input = Input(shape=(x_train[0].shape, 1))
-        model_conv_11 = Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(model_input)
-        model_pool_11 = MaxPooling2D(pool_size=(2, 2))(model_conv_11)
-        model_conv_12 = Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(model_pool_11)
-        model_pool_12 = MaxPooling2D(pool_size=(2, 2))(model_conv_12)
-        model_flatten_12 = Flatten()(model_pool_12)
-        # Output sofmax layer
-        predictions = Dense((1), activation='sigmoid')(model_flatten_12)
-        model_conv = Model(inputs=[model_input], output=predictions)
-        model_conv.compile(optimizer='rmsprop',
-                           loss='binary_crossentropy',
-                           metrics=['accuracy'])
-        model_conv.fit(x_train, y_train, epochs=epochs, batch_size=32, verbose=1)
-        return model_conv
+    def save_model(model, test_df):
+        x = np.asarray(test_df['x'].tolist())
+        y = np.asarray(test_df['y'].tolist())
+        scores = model.evaluate(x, y, verbose=1)
+        print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
+    def load_model(model, test_df):
+        x = np.asarray(test_df['x'].tolist())
+        y = np.asarray(test_df['y'].tolist())
+        scores = model.evaluate(x, y, verbose=1)
+        print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
-def train_2model(x_train, y_train, epochs):
-    # FFT conv layers
-    train_samle_length = len(x_train[0])
-    print(train_samle_length)
-    print(x_train[0].shape)
-    if K.image_data_format() == 'channels_first':
-        input_shape = (3, 393, 393)
-    else:
-        input_shape = (393, 393, 3)
-
-    model_input = Input(shape=input_shape)
-    # model_input = Input(shape=(x_train[0].shape, 1))
-    model_conv_11 = Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(model_input)
-    model_pool_11 = MaxPooling2D(pool_size=(2, 2))(model_conv_11)
-    model_conv_12 = Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(model_pool_11)
-    model_pool_12 = MaxPooling2D(pool_size=(2, 2))(model_conv_12)
-    model_conv_13 = Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(model_pool_12)
-    model_pool_13 = MaxPooling2D(pool_size=(2, 2))(model_conv_13)
-    model_flatten_14 = Flatten()(model_pool_13)
-    model_dense_15 = Dense((64), activation='relu')(model_flatten_14)
-    model_dropout_16 = Dropout(0.2)(model_dense_15)
-    # Output sofmax layer
-    predictions = Dense((1), activation='sigmoid')(model_dropout_16)
-    model_conv = Model(inputs=[model_input], output=predictions)
-    model_conv.compile(optimizer='rmsprop',
-                       loss='binary_crossentropy',
-                       metrics=['accuracy'])
-    model_conv.fit(x_train, y_train, epochs=epochs, batch_size=20, verbose=1)
-    return model_conv
-
-
-def test_model(model, test_df):
-    x = np.asarray(test_df['x'].tolist())
-    y = np.asarray(test_df['y'].tolist())
-    scores = model.evaluate(x, y, verbose=1)
-    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
-
-def save_model(model, test_df):
-    x = np.asarray(test_df['x'].tolist())
-    y = np.asarray(test_df['y'].tolist())
-    scores = model.evaluate(x, y, verbose=1)
-    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
-
-def load_model(model, test_df):
-    x = np.asarray(test_df['x'].tolist())
-    y = np.asarray(test_df['y'].tolist())
-    scores = model.evaluate(x, y, verbose=1)
-    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    def test_model_loop(self, model):
+        predicted_list = []
+        for i in range(self.test_df_xs.shape[0]):
+            # print(self.test_df_xs.loc[i].values)
+            result = self.predict_model(model, self.test_df_xs.loc[i].values)
+            predicted_list.append(result)
+        d = {'predicted': predicted_list, 'anoted': self.test_df_ys.values}
+        result_df = DataFrame(data=d)
+        result_df.to_excel("output.xlsx")
+        print(result_df)
 
 
 if __name__ == "__main__":
     square_series_folder = PurePath(os.getcwd(), 'square_db')
     tringle_series_folder = PurePath(os.getcwd(), 'triangle_db')
     df_arff2_filename = PurePath(os.getcwd(), 'squre_triangle').with_suffix('.arff')
-    df = CreateDf()
-    df.create_df_from_folders([square_series_folder, tringle_series_folder], ['sq', 't'])
-    df.save_to_arff2(df_arff2_filename)
-    df.load_from_arff2(df_arff2_filename)
-    print(df.get_df())
-    # SynthANN_o = SynthANN(square_series_folder, tringle_series_folder)
-    # SynthANN_o.normalize_df()
-    # train, test = train_test_split(df, test_size=0.2, shuffle=True)
-    # history, model = train_model(train, epochs_to_train=100)
-    # test_model(model, test)
+    dfo = CreateDf()
+    # df.create_df_from_folders([square_series_folder, tringle_series_folder], ['0', '1'])
+    # df.save_to_arff2(df_arff2_filename)
+    dfo.load_from_arff2(df_arff2_filename)
+    dfd = dfo.get_df()
+    anno = SynthANN(dfd)
+    anno.truncate_data(rows_to_store=200)
+    # print(anno.df)
+    anno.split_data()
+    anno.normalize_df()
+    # print(anno.ndf_train_x)
+    # print(anno.ndf_train_y)
+    model = anno.get_model()
+    scores = anno.test_model(model)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
